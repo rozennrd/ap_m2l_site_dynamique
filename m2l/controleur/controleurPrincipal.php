a9837c9d2ccbe9cf14b9e42542ca4a0f4fb7423f
unknown <r_renaud@gmx.fr>
2023-01-04 11:04:49 +0100

    fonctionnalité connexion ok

--- m2l/controleur/controleurPrincipal.php
+++ m2l/controleur/controleurPrincipal.php
@@ -4,43 +4,37 @@ require_once "modele/dto/Utilisateur.php";
 require_once "modele/dao/UtilisateurDAO.php";
 
 /***** CREATION / GESTION DE LA SESSION *****/
+/**
+ * Pour gérer un utilisateur connecté, utiliser la variable $utilisateurActuel. 
+ * Si cette variable existe et n'est pas nulle, l'utilisateur est connecté ! 
+ * Plus qu'à utiliser sa commande getStatut() pour vérifier que l'utilisateur possède le bon statut. 
+ */
 
-/*if(isset($_GET['identification'])){
-	$_SESSION['identification']= $_GET['identification'];
-}
-else
-{
-	if(!isset($_SESSION['identification'])){
-		$_SESSION['identification']=False;
-	}
-}*/
+
 echo "<script> alert(". $_SESSION['identification'].";)</script>";
 $messageConnexion = '';
-//echo "<script>alert(\""+$_SESSION['identification']+"\")</script>";
+
 echo isset($_POST['login']);
 echo implode(", ", $_POST);
 echo "bonjour";
 if(isset($_POST['login']))
 {
-	echo "on est dans le logiiiin";
+	
 	// Création d'un utilisateur + irrigation avec le login et le mdp récupérés
 	$unUtilisateur = new Utilisateur();
 	$unUtilisateur->setLogin($_POST['login']); 
 	$unUtilisateur->setMdp($_POST['mdp']);
 	
 	$_SESSION['identification'] = UtilisateurDAO::verification($unUtilisateur); // Vérification 
-	echo "<script> alert(".$_SESSION['identification'].";)</script>";
+	$utilisateurActuel = UtilisateurDAO::getUtilisateur($_SESSION['identification']);
 	if (isset($_SESSION['identification']) && $_SESSION['identification'])
 	{
-		var_dump("test0".$_SESSION['identification']);
 		$_GET['m2lMP'] = "accueil";
 		$messageConnexion = "Vous êtes connecté";
-		
 		// Insérer ici vos modifications / les choses qui dépendent de si votre utilisateur est identifié
 	}
 	else
 	{
-		var_dump("test1".$_SESSION['identification']);
 		$messageConnexion = "Login ou mot de passe incorrect";
 	}
 }
@@ -63,23 +57,6 @@ else
 	}
 }
 
-// TODO : Tester la connexion 
-
-
-// Comment gérer l'identification ?
-if (isset($_SESSION['identification']) && $_SESSION["identification"]) // S'il existe une valeur d'identification dans la session && qu'elle n'est pas nulle 
-{
-	// Insérer les choses qui nécessitent d'être identifié
-
-	// Gestion de l'item de connexion du menu
-	$texteItemConnexion = "Se déconnecter"; 
-
-}
-else 
-{
-	// Gestion de l'item de connexion du menu
-	$texteItemConnexion = "Se connecter"; 
-}
 // TODO : Tester la connexion 
 
 
--- m2l/modele/dao/UtilisateurDAO.php
+++ m2l/modele/dao/UtilisateurDAO.php
@@ -3,19 +3,28 @@ require_once "modele/dao/dBConnex.php";
 
 class UtilisateurDAO {
     public static function verification(Utilisateur $unUtilisateur) {
-        echo "bonjour on vérifie l'utilisateur";
         
         $requetePrepa = dBConnex::getInstance()->prepare("SELECT idUser FROM utilisateur WHERE login=:login AND mdp=:mdp;");
         $login = $unUtilisateur->getLogin(); 
         $mdp = $unUtilisateur->getMdp(); // Dans l'idéal, à sécuriser avec la fonction hash("sha512", mot_de_passe) + sécuriser lors de l'enregistrement en bdd. 
-        echo "<script>alert(\" $login, $mdp\");</script>";
         $requetePrepa->bindParam(":login", $login);
         $requetePrepa->bindParam(":mdp", $mdp);
         $requetePrepa->execute();
         $idUser = $requetePrepa->fetch();
-        
-        echo "<script>alert(\" $login, $mdp\");</script>";
         return $idUser[0];
     }
+
+    public static function getUtilisateur($unIdUtilisateur) {
+        $requetePrepa = dBConnex::getInstance()->prepare("SELECT * FROM utilisateur WHERE id=:id");
+        $requetePrepa->bindParam(":id", $unIdUtilisateur);
+        $requetePrepa->execute();
+        $utilisateurTab = $requetePrepa->fetch();
+
+        $utilisateur = new Utilisateur();
+        $utilisateur->hydrate($utilisateurTab);
+        return $utilisateur; 
+    }
+
+
 
 }
